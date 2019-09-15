a598947391b3510ccd3247226003e6af618f564f
Luiz Rodrigo Schuitek <rodrigoschuitek@gmail.com>
2019-09-15 12:53:39 -0300

    Marcar notificação como lida.

--- src/routes.js
+++ src/routes.js
@@ -38,5 +38,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 // Notificação
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
